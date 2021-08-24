a27062a8b1d99a24820aeb9f16fb4c0f979232cb
florian olhasque <olhasque.florian@gmail.com>
2021-08-24 16:12:01 +0200

    availability crud

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,2 +1,5 @@
 class AvailabilitiesController < ApplicationController
+  def index
+    @availability = Availabilitie.where(user_id: current_user.id)
+  end
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @user = User.find(:user_id)
+    @mission = Mission.find(:mission_id)
+    @booking.user = @user
+    @booking.mission = @mission
+    if @booking.save
+      redirect_to availabilities_path
+    else
+      render new
+    end
+  end
 end
